d93b01547dec2a0751acbcf877727ad27517780f
CapralDavid <55207583+CapralDavid@users.noreply.github.com>
2021-10-09 23:01:59 +0200

    Add files via upload

---
+++ server.go
@@ -0,0 +1,37 @@
+
+
+package main
+
+import (
+    "encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+    "os"
+	"time"
+)
+
+
+func main() {
+	http.HandleFunc("/time", main1)
+    log.Fatal(http.ListenAndServe(":8201", nil))
+
+}
+type User struct {
+   Time     time.Time
+}
+
+func main1(w http.ResponseWriter, r *http.Request) {
+
+    u := &User{
+        Time: time.Now(),
+    }
+
+    out, err := json.MarshalIndent(u, "", "  ")
+    if err != nil {
+        log.Println(err)
+        os.Exit(1)
+    }
+
+    fmt.Println(string(out))
+}
